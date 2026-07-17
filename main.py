f7d20cea4f13c8f9822e56264cb7c60f2a1f577d
Caio Kazu <caioitaya08@gmail.com>
2026-07-16 22:40:28 -0400

    Feat: Translation of any values to english

--- main.py
+++ main.py
@@ -35,9 +35,9 @@ def login_route(
     db: Session = Depends(get_db), form_data: OAuth2PasswordRequestForm = Depends()
 ):
     """Rota de login do sistema"""
-    nome_login = form_data.username
-    senha_login = form_data.password
-    return login(db=db, username=nome_login, senha=senha_login)
+    login_name = form_data.username
+    login_password = form_data.password
+    return login(db=db, username=login_name, senha=login_password)
 
 
 PASTA_UPLOADS = Path("uploads")
@@ -47,9 +47,9 @@ PASTA_UPLOADS.mkdir(exist_ok=True)
 
 @app.post("/cursos/{curso_id}/aulas")
 def receive_files_endpoint(
-    curso_id: int,
-    titulo_aula: str = Form(...),
-    arquivo_upload: UploadFile = File(...),
+    course_id: int,
+    class_title: str = Form(...),
+    file_upload: UploadFile = File(...),
     username_login: str = Depends(verificar_token),
     db: Session = Depends(get_db),
 ):
@@ -57,57 +57,57 @@ def receive_files_endpoint(
 
     return adicionar_aula_curso(
         db=db,
-        arquive=arquivo_upload,
-        curso_id=curso_id,
+        arquive=file_upload,
+        curso_id=course_id,
         nome_usuario=username_login,
-        titulo_aula=titulo_aula,
+        titulo_aula=class_title,
     )
 
 
 @app.post("/registro/aluno")
 def create_student_endpoint(
-    estudante_registro: UsuarioValidar, db: Session = Depends(get_db)
+    student_register: UsuarioValidar, db: Session = Depends(get_db)
 ):
     """Rota publica para registro de estudantes"""
 
-    return criar_estudante(db=db, estudante=estudante_registro)
+    return criar_estudante(db=db, estudante=student_register)
 
 
 @app.post("/registro/instrutor")
 def create_instructor_endpoint(
-    instrutor_registro: UsuarioValidar,
+    register_of_instructor: UsuarioValidar,
     username_login: str = Depends(verificar_token),
     db: Session = Depends(get_db),
 ):
     """Rota privada para admnistradores registrarem novos instrutores para o site"""
     return criar_instrutor(
-        instrutor=instrutor_registro, confirmacao_login=username_login, db=db
+        instrutor=register_of_instructor, confirmacao_login=username_login, db=db
     )
 
 
 @app.post("/registro/curso")
 def create_course_endpoint(
-    curso_registro: CursosValidar,
+    registration_course: CursosValidar,
     username_login: str = Depends(verificar_token),
     db: Session = Depends(get_db),
 ):
     """Rota privada a qual apenas o instrutor pode criar o curso"""
 
     return criar_curso(
-        confirmacao_login=username_login, dados_curso=curso_registro, db=db
+        confirmacao_login=username_login, dados_curso=registration_course, db=db
     )
 
 
 @app.get("/curso/matricula")
 def create_registration_endpoint(
-    nome_curso: str,
+    course_name: str,
     db: Session = Depends(get_db),
     username_login: str = Depends(verificar_token),
 ):
     """Rota privada para estudantes  para registrar a matricula em alguma aula"""
 
     return criar_matricula(
-        confirmacao_login=username_login, curso_nome=nome_curso, db=db
+        confirmacao_login=username_login, curso_nome=course_name, db=db
     )
 
 
@@ -118,16 +118,16 @@ def list_courses_endpoint(db: Session = Depends(get_db)):
 
 
 @app.get("/cursos/{curso_id}/aulas")
-def list_classes_endpoint(curso_id: int, db: Session = Depends(get_db)):
+def list_classes_endpoint(course_id: int, db: Session = Depends(get_db)):
     """Lista as aulas em formato JSON"""
-    return listar_aulas_do_curso(db=db, curso_id=curso_id)
+    return listar_aulas_do_curso(db=db, curso_id=course_id)
 
 
 @app.get("/aulas/{aula_id}/download")
 def download_class_endpoint(
-    aula_id: int,
+    class_id: int,
     db: Session = Depends(get_db),
     login_str: str = Depends(verificar_token),
 ):
     """Rota privada: o aluno precisa estar logado para baixar o arquivo da aula"""
-    return baixar_arquivo_aula(aula_id=aula_id, db=db)
+    return baixar_arquivo_aula(aula_id=class_id, db=db)
