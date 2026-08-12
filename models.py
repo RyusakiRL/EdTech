cd1b7c87bb819c3dfdb9b7afe3745850dba1ea10
Caio Kazu <caioitaya08@gmail.com>
2026-08-11 22:11:08 -0400

    refactor: initializing refactor scope

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 """Table creation templates in SQL"""
 
 from sqlalchemy.orm import declarative_base, relationship
-from sqlalchemy import Integer, String, ForeignKey, Float, Column
+from sqlalchemy import Integer, String, ForeignKey, Column, DateTime, Float, Boolean
+from datetime import datetime, timezone
 
 Base = declarative_base()
 
@@ -11,64 +12,85 @@ class User(Base):
 
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
-    name_user = Column(String, nullable=False, unique=True, index=True)
+    name_user = Column(String, nullable=False, unique=False, index=True)
     password_user = Column(String, nullable=False)
     role_user = Column(String, nullable=False)
-    my_number = Column(Integer, nullable=False)
-    created_courses_relationship = relationship(
-        "Course", back_populates="instructor_relationship"
+    my_number = Column(Integer, nullable=False, unique=True)
+    is_active = Column(Boolean, nullable=False, default=True)
+    manager_department_relationship = relationship(
+        "Department", back_populates="manager_relationship"
     )
-    enrollments_relationship = relationship(
-        "Registration", back_populates="student_relationship"
+    employee_timerecord_relationship = relationship(
+        "TimeRecord", back_populates="employee_relationship"
+    )
+    payment_relationship = relationship(
+        "MonthlyPayroll", back_populates="user_payment_relationship"
     )
 
 
-class Course(Base):
-    """Template for courses creation"""
+class Department(Base):
+    """Template for Department creation"""
 
-    __tablename__ = "courses"
+    __tablename__ = "departments"
     id = Column(Integer, primary_key=True, index=True)
-    course_title = Column(String, nullable=False)
-    description = Column(String, nullable=False)
-    id_instructor = Column(Integer, ForeignKey("users.id"), nullable=False)
-
-    instructor_relationship = relationship(
-        "User", back_populates="created_courses_relationship"
+    id_manager = Column(Integer, ForeignKey("users.id"), nullable=False)
+    department_title = Column(String, nullable=False)
+    manager_relationship = relationship(
+        "User", back_populates="manager_department_relationship"
     )
-    enrollment_students_relationship = relationship(
-        "Registration", back_populates="course_relationship"
-    )
-    classes_relationship = relationship(
-        "Lesson", back_populates="course_class_relationship"
+    inventory_relationship = relationship(
+        "DailyInventory", back_populates="department_relationship"
     )
 
 
-class Lesson(Base):
-    """Table template for file path creation"""
+class TimeRecord(Base):
+    """Table template for time record creation"""
 
-    __tablename__ = "classes"
+    __tablename__ = "time_records"
+    id = Column(
+        Integer,
+        primary_key=True,
+    )
+    employee_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+
+    clock_in = Column(DateTime, nullable=False)
+    clock_out = Column(DateTime, nullable=True)
+    hour_worked = Column(Float, nullable=True)
+    employee_relationship = relationship(
+        "User", back_populates="employee_timerecord_relationship"
+    )
+
+
+class MonthlyPayroll(Base):
+    """Table template for monthly payment"""
+
+    __tablename__ = "monthly_payroll"
     id = Column(Integer, primary_key=True, index=True)
-    classes_title = Column(String, nullable=False)
-    file_path_class = Column(String, nullable=False)
-    course_id = Column(Integer, ForeignKey("courses.id"))
-
-    course_class_relationship = relationship(
-        "Course", back_populates="classes_relationship"
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
+    datetime_payment = Column(DateTime, nullable=False)
+    salary = Column(Float, nullable=False)
+    user_payment_relationship = relationship(
+        "User", back_populates="payment_relationship"
     )
 
 
-class Registration(Base):
-    """Table template for enrollment creation"""
+class DailyInventory(Base):
+    """Table template for enterprise products inventory"""
 
-    __tablename__ = "enrollments"
+    __tablename__ = "daily_inventory"
     id = Column(Integer, primary_key=True, index=True)
-    progress = Column(Float, default=0.0)
-    student_id = Column(Integer, ForeignKey("users.id"))
-    course_id = Column(Integer, ForeignKey("courses.id"))
-
-    student_relationship = relationship(
-        "User", back_populates="enrollments_relationship"
+    department_id = Column(Integer, ForeignKey("departments.id"), nullable=False)
+    created_at = Column(
+        DateTime, nullable=False, default=lambda: datetime.now(timezone.utc)
     )
-    course_relationship = relationship(
-        "Course", back_populates="enrollment_students_relationship"
+    updated_at = Column(
+        DateTime,
+        default=lambda: datetime.now(timezone.utc),
+        onupdate=lambda: datetime.now(timezone.utc),
+    )
+    product_in_stock = Column(Integer, nullable=False)
+    product_to_come = Column(Integer, nullable=False)
+    product_removed = Column(Boolean, default=False)
+    department_relationship = relationship(
+        "Department", back_populates="inventory_relationship"
     )
