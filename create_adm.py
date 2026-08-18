05e7f90fee8aab0b5c572ac9a40781c57c273e85
Caio Kazu <caioitaya08@gmail.com>
2026-08-18 10:22:48 -0400

    refactor: only refactor for this system, but same function and bug fix

--- create_adm.py
+++ create_adm.py
@@ -1,27 +1,37 @@
 """Create a admnistrator for the system, to check any database"""
 
 from sqlalchemy.orm import Session
-from models import User
-from schemas import UserValidation
+from models import User, Status
+from schemas import ManagerAdministratorValidation
 from security import generator_hash_password
 from database import get_db
 
-ROLE = "administrator"
 ADM_NAME = str(input("Input the name of new system administrator: "))
 ADM_PASSWORD = str(input("Input the password of new system administrator: "))
-new_administrator = UserValidation(user_name=ADM_NAME, password_model=ADM_PASSWORD)
+ADM_NUMBER = int(input("Input the number of new system administrator: "))
+new_administrator = ManagerAdministratorValidation(
+    name_user=ADM_NAME, password_model=ADM_PASSWORD, my_number=ADM_NUMBER
+)
 db = next(get_db())
 
 
-def new_server_administrator(new_adm: UserValidation, db_session: Session):
+def new_server_administrator(
+    new_adm: ManagerAdministratorValidation, db_session: Session
+):
     """Create a new admnistrator in the system"""
-    if db_session.query(User).filter(User.name_user == new_adm.user_name).first():
-        print("This name already exists input a other name")
+    adm_existence = (
+        db_session.query(User).filter(User.my_number == new_adm.my_number).first()
+    )
+    if adm_existence and adm_existence.is_active is True:
+        print("This number already exists input a other number")
     else:
         hashed_password = generator_hash_password(new_adm.password_model)
 
         creation_of_new_administrator = User(
-            name_user=new_adm.user_name, password_user=hashed_password, role_user=ROLE
+            name_user=new_adm.name_user,
+            password_user=hashed_password,
+            role_user=Status.ADMINISTRATOR,
+            my_number=new_adm.my_number,
         )
 
         db_session.add(creation_of_new_administrator)
